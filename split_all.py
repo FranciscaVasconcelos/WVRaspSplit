e682daa2310ad3303b95a37a888419e3dd418f77
Francisca Vasconcelos <frannycisca@gmail.com>
2016-07-04 15:47:46 +0200

    Script to crop all raspberry phtos in a directory

--- split_all.py
+++ split_all.py
@@ -8,7 +8,7 @@ cdir = input("current_dir: ")
 files = os.listdir(cdir)
 
 p = []
-for i in files:
+for i in sorted(files):
     p.append(cdir+'//'+i)
 
 columns = int(input("# berry columns: "))
@@ -24,10 +24,11 @@ for n in range(len(p)):
     im = Image.open(p[n])
     crop_col = int(im.size[0]/columns)
     crop_row = int(im.size[1]/rows)
-
+    m=0
     for i in range(0,columns):
         for j in range(0,rows):
             tmp = im.crop((i*crop_col,j*crop_row,i*crop_col+crop_col,j*crop_row+crop_row))
             tmp.load()
-            tmp.save(directory+"/"+str(n)+'_'+str(i)+str(j)+".jpg")
+            tmp.save(directory+"/"+str(n+1)+'_'+str(m)+".jpg")
+            m += 1
         
